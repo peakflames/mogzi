ce17b3046c28795d112c5b939083e1ef17f23b71
Todd Schavey <tschavey@archer.com>
2025-03-05 16:00:59 -0500

    initial commit

---
+++ CliChat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ClientModel;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+const string API_KEY = "SOME_API_KEY";
+
+IChatClient client = new OpenAIClient(
+    new ApiKeyCredential(API_KEY),
+    new OpenAIClientOptions { 
+        Endpoint = new("SOME_ENDPOINT_URL")
+    })
+    .AsChatClient("claude-3-7-sonnet");
+
+
+List<ChatMessage> conversation =
+[
+    new(ChatRole.System, "You are a helpful AI assistant"),
+    new(ChatRole.User, "Hello! How can you help me today?"),
+];
+
+await foreach (var update in client.GetStreamingResponseAsync(conversation))
+{
+    Console.Write(update);
+}
+
+
+Console.WriteLine();
---
+++ README.md
@@ -0,0 +1,13 @@
+# Chat - One-Shot Chat (Straaming)
+
+This example demonstrates how to use the OpenAI client to stream a chat response.
+
+The example was created using the following steps:
+
+1. dotnet new console -n CliChat
+1. dotnet add package Microsoft.Extensions.AI --prerelease
+1. dotnet add package Microsoft.Extensions.AI.Abstractions --prerelease
+1. dotnet add package Microsoft.Extensions.AI.OpenAI --prerelease
+1. Update the Program.cs using example from the [dotnet ai-samples Github repo](https://github.com/dotnet/ai-samples/blob/main/src/microsoft-extensions-ai/openai/OpenAIExamples/Streaming.cs) for MEAI
+1. dotnet run
+
